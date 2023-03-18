65e537e6c050b0e0af33b139cad2344acb4ee104
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-03-18 21:42:55 +0000

    Add MUI

---
+++ src/App.styles.ts
@@ -0,0 +1,10 @@
+import { css } from "@emotion/react";
+
+export const GlobalStyles = css`
+  body,
+  html,
+  #root {
+    margin: 0;
+    padding: 0;
+  }
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { Global } from "@emotion/react";
+import { GlobalStyles } from "./App.styles";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 export const App = () => {
-  return <div>TODO</div>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Global styles={GlobalStyles} />
+      <CssBaseline />
+      <div>TODO</div>
+    </ThemeProvider>
+  );
 };
