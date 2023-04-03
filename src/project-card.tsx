45ef86707f1b4a1bcf32d563eaecdc3be848ba85
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2023-04-03 22:32:16 +0100

    Wrap CardMedia component in picture element to provide webp and png support

--- src/project-card.tsx
+++ src/project-card.tsx
@@ -8,6 +8,19 @@ import {
 } from "@mui/material";
 import { AspectRatio } from "react-aspect-ratio";
 
+const makeImagePath = (path: string, extname: string): string => {
+  const dotIndex = path.lastIndexOf(".");
+  const basename = dotIndex >= 0 ? path.substring(0, dotIndex) : path;
+  return `${process.env.PUBLIC_URL}/${basename}.${extname}`;
+};
+
+const makePictureSource = (imagePath: string): JSX.Element | undefined => {
+  const dotIndex = imagePath.lastIndexOf(".");
+  if (dotIndex < 0) return undefined;
+  const type = imagePath.substring(dotIndex + 1);
+  return <source srcSet={imagePath} type={`image/${type}`} />;
+};
+
 type CardProps = {
   project: Project;
   onLearnMore: (project: Project) => void;
@@ -17,6 +30,9 @@ export const ProjectCard: React.FunctionComponent<CardProps> = ({
   project,
   onLearnMore,
 }: CardProps) => {
+  const webpCardImagePath = makeImagePath(project.cardImage.path, "webp");
+  const pngCardImagePath = makeImagePath(project.cardImage.path, "png");
+
   return (
     <Card variant="outlined" sx={{ backgroundColor: "#444" }}>
       <CardActionArea
@@ -25,10 +41,15 @@ export const ProjectCard: React.FunctionComponent<CardProps> = ({
       >
         <CardContent sx={{ height: "100%" }}>
           <AspectRatio ratio="16/9">
-            <CardMedia
-              image={`${process.env.PUBLIC_URL}/${project.cardImage.path}`}
-              title={project.cardImage.caption}
-            />
+            <picture>
+              {makePictureSource(webpCardImagePath)}
+              {makePictureSource(pngCardImagePath)}
+              <CardMedia
+                component="img"
+                image={pngCardImagePath}
+                title={project.cardImage.caption}
+              />
+            </picture>
           </AspectRatio>
           <Typography gutterBottom variant="h6">
             {project.title}
